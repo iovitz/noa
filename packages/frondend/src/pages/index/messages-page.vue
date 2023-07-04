2fe9ce6c719ea3e34d2228997d77230de08da3b6
iovitz <iovitz1999@gmail.com>
2023-07-04 23:08:50 +0800

    feat: 优化消息列表

--- packages/frondend/src/pages/index/messages-page.vue
+++ packages/frondend/src/pages/index/messages-page.vue
@@ -9,27 +9,17 @@
 			<uni-icons color="#999999" size="18" type="search" />
 		</template>
 	</uni-search-bar>
-	<uni-list :border="false">
-		<uni-list-chat
-			v-for="i in 99"
-			:key="i"
-			title="xieqianyu"
-			:avatar-circle="true"
-			avatar="https://web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-			note="您收到一条新的消息"
-			time="2020-02-02 20:20"
-			badge-positon="left"
-			badge-text="99"
-			clickable
-		/>
-	</uni-list>
+	<message-list></message-list>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import messageList from '@/comps/message-list/message-list.vue';
 import logger from '@/utils/logger';
 
 export default defineComponent({
-	components: {},
+	components: {
+		messageList,
+	},
 	methods: {
 		handleGoSearch() {
 			logger.verbose('前往搜索');
