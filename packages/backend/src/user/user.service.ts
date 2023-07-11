b42ccfe22f5aeb5f5a7a83f3166e4133361697af
iovitz <iovitz1999@gmail.com>
2023-07-11 23:22:09 +0800

    fix: 修复0开头userid的bug

--- packages/backend/src/user/user.service.ts
+++ packages/backend/src/user/user.service.ts
@@ -10,9 +10,9 @@ export class UserService {
 
   genNumberString(digit = 10) {
     const str = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    let res = '';
-    for (let i = 0, len = str.length; i < digit; i++) {
-      const id = Math.floor(Math.random() * len);
+    let res = str[Math.floor(Math.random() * 9)];
+    for (let i = 0; i < digit; i++) {
+      const id = Math.floor(Math.random() * 10);
       res += str[id];
     }
     return res;
