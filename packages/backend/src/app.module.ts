7861f25bb03f3e93565e8872285caac58ebe7db2
iovitz <iovitz1999@gmail.com>
2023-08-04 19:35:48 +0800

    feat: 添加moments模块

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -3,8 +3,9 @@ import { UserModule } from './user/user.module';
 import { GlobalModule } from './global/common.module';
 import { LogModule } from './log/log.module';
 import { WsModule } from './ws/ws.module';
+import { MomentsModule } from './moments/moments.module';
 
 @Module({
-  imports: [GlobalModule, UserModule, LogModule, WsModule],
+  imports: [GlobalModule, UserModule, LogModule, WsModule, MomentsModule],
 })
 export class AppModule {}
