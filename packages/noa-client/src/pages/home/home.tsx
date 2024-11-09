616c9fbd39191f26fa5328dfaccbeaa1169c565f
iovitz <iovitz1999@gmail.com>
2024-11-09 23:53:21 +0800

    chore: 格式化

--- packages/noa-client/src/pages/home/home.tsx
+++ packages/noa-client/src/pages/home/home.tsx
@@ -1,7 +1,6 @@
 import PageHeader from '@/components/header/header'
 import { Layout } from 'antd'
-import { Content, Header } from 'antd/es/layout/layout'
-import { ItemType, MenuItemType } from 'antd/es/menu/interface'
+import { Content } from 'antd/es/layout/layout'
 import React from 'react'
 
 export default function Home() {
