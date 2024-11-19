d6e271c946676bd89bda60a8ec6d3278f47f275b
iovitz <iovitz1999@gmail.com>
2024-11-19 21:16:21 +0800

    chore: 格式化

--- packages/noa-server/src/sqlite/oauth.entity.ts
+++ packages/noa-server/src/sqlite/oauth.entity.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm'
 
 @Entity('oauth')
 export class OAuth {
