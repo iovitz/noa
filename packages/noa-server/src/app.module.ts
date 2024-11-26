0d111d472503f140981120f3e463c20da727679a
iovitz <iovitz1999@gmail.com>
2024-11-26 19:23:33 +0800

    feat: 自定义Typeorm的Logger

--- packages/noa-server/src/app.module.ts
+++ packages/noa-server/src/app.module.ts
@@ -6,6 +6,7 @@ import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core'
 import { EventEmitterModule } from '@nestjs/event-emitter'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import * as cookieParser from 'cookie-parser'
+import { Logger } from 'typeorm'
 import { BadRequestFilter } from './aspects/filters/bad-request/bad-request.filter'
 import { DefaultFilter } from './aspects/filters/default/default.filter'
 import { HttpFilter } from './aspects/filters/http/http.filter'
@@ -27,16 +28,38 @@ import { UtilModule } from './util/util.module'
   imports: [
     UtilModule,
     TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: async (_configService: ConfigService) => {
+      inject: [TracerService],
+      useFactory: async (tracerService: TracerService) => {
         // 放到主页目录下
         const sqliteFilePath = join(homedir(), 'sqlite/noa.sqlite')
+        const typeormTracer = tracerService.child('typeorm')
+        const typeOrmLogger: Logger = {
+          logQuery(query: string) {
+            typeormTracer.debug(query)
+          },
+          logQueryError(error: string | Error, query: string) {
+            typeormTracer.error(query, error)
+          },
+          logQuerySlow(time: number, query: string) {
+            typeormTracer.log('Query Slow', { time, query })
+          },
+          logSchemaBuild(message: string) {
+            typeormTracer.debug(`Schema Build: ${message}`)
+          },
+          logMigration(message: string) {
+            typeormTracer.debug(`Schema Build: ${message}`)
+          },
+          log(level: 'log' | 'info' | 'warn', message: any) {
+            typeormTracer.debug('LOG', message)
+          },
+        }
         return {
           type: 'better-sqlite3',
           database: sqliteFilePath,
           autoLoadEntities: true,
           synchronize: true,
           logging: true,
+          logger: typeOrmLogger,
         }
       },
     }),
