10e6a0dbbdae243b491f1a904402fe1551a57f64
iovitz <iovitz1999@gmail.com>
2024-11-26 16:38:46 +0800

    chore: 删除测试代码

--- packages/noa-server/src/status/status.controller.ts
+++ packages/noa-server/src/status/status.controller.ts
@@ -3,7 +3,6 @@ import { Controller, Get, Render } from '@nestjs/common'
 @Controller('api-noa/status')
 export class StatusController {
   @Get()
-  @Render('io')
   getStatus() {
     return 'Hello, world'
   }
