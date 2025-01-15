a39a19ce7dd325434796c929cf312ab9aa5ea6e8
iovitz <iovitz1999@gmail.com>
2025-01-15 15:18:30 +0800

    feat: 开发环境使用MockRedis

--- packages/noa-server/src/redis/redis.module.ts
+++ packages/noa-server/src/redis/redis.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import Redis from 'ioredis'
+import RedisMock from 'ioredis-mock'
 import { TracerService } from 'src/util/tracer/tracer.service'
 
 export const REDIS_CLIENT = Symbol('REDIS_CLIENT')
@@ -11,7 +12,7 @@ export const REDIS_CLIENT = Symbol('REDIS_CLIENT')
     provide: REDIS_CLIENT,
     inject: [ConfigService, TracerService],
     useFactory: async (configService: ConfigService, tracerService: TracerService) => {
-      const redis = new Redis(configService.get('REDIS_CONNECT_URL'))
+      const redis = configService.get('isProd') ? new Redis(configService.get('REDIS_CONNECT_URL')) : new RedisMock()
       await redis.ping() // ping 方法用于测试连接是否正常
       tracerService.log('Redis Ready')
       return redis
